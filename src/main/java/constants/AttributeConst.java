b66654ad0874979b70b00be64ffa925711073128
Ren.Saito <rsaito@unisrv.jp>
2023-06-27 18:33:37 +0900

    管理者チェックを追加

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -16,10 +16,10 @@ public enum AttributeConst {
     ERR("errors"),
 
     // ログイン中の従業員
-    LOGIN_EMP("logon_employee"),
+    LOGIN_EMP("login_employee"),
 
     // ログイン画面
-    LOGIN_ERR("loginEroor"),
+    LOGIN_ERR("loginError"),
 
     // 従業員管理
     EMPLOYEE("employee"),
