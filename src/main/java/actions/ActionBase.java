7e67d3811799d7a827647213291dbafd3d5b5e3a
Ren.Saito <rsaito@unisrv.jp>
2023-06-27 14:40:57 +0900

    従業員indexアクションの作成

--- src/main/java/actions/ActionBase.java
+++ src/main/java/actions/ActionBase.java
@@ -15,23 +15,22 @@ import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.PropertyConst;
 
-/*
+/**
  * 各Actionクラスの親クラス。共通処理を行う。
+ *
  */
-
 public abstract class ActionBase {
     protected ServletContext context;
     protected HttpServletRequest request;
     protected HttpServletResponse response;
 
-    /*
+    /**
      * 初期化処理
      * サーブレットコンテキスト、リクエスト、レスポンスをクラスフィールドに設定
      * @param servletContext
      * @param servletRequest
      * @param servletResponse
      */
-
     public void init(
             ServletContext servletContext,
             HttpServletRequest servletRequest,
@@ -41,55 +40,61 @@ public abstract class ActionBase {
         this.response = servletResponse;
     }
 
-    /*
+    /**
      * フロントコントローラから呼び出されるメソッド
      * @throws ServletException
      * @throws IOException
      */
     public abstract void process() throws ServletException, IOException;
 
-    /*
+    /**
      * パラメータのcommandの値に該当するメソッドを実行する
      * @throws ServletException
      * @throws IOException
      */
     protected void invoke()
-        throws ServletException, IOException {
-            Method commandMethod;
-            try {
-                // パラメータからcommandを取得
-                String command = request.getParameter(ForwardConst.CMD.getValue());
+            throws ServletException, IOException {
 
-                // commandに該当するメソッドを実行する
-                //(例: action=Employee command=show の場合 EmployeeActionクラスのshow()メソッドを実行する)
-                commandMethod = this.getClass().getDeclaredMethod(command, new Class[0]);
-                commandMethod.invoke(this, new Object[0]); // メソッドに渡す引数はなし
-            } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
-                    | InvocationTargetException | NullPointerException e) {
-                // 発生した例外をコンソールに表示
-                e.printStackTrace();
-                // commandの値が不正で実行できない場合エラー画面を呼び出し
-                forward(ForwardConst.FW_ERR_UNKNOWN);
-            }
+        Method commandMethod;
+        try {
+
+            //パラメータからcommandを取得
+            String command = request.getParameter(ForwardConst.CMD.getValue());
+
+            //ommandに該当するメソッドを実行する
+            //(例: action=Employee command=show の場合 EmployeeActionクラスのshow()メソッドを実行する)
+            commandMethod = this.getClass().getDeclaredMethod(command, new Class[0]);
+            commandMethod.invoke(this, new Object[0]); //メソッドに渡す引数はなし
+
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException | NullPointerException e) {
+
+            //発生した例外をコンソールに表示
+            e.printStackTrace();
+            //commandの値が不正で実行できない場合エラー画面を呼び出し
+            forward(ForwardConst.FW_ERR_UNKNOWN);
         }
 
-    /*
+    }
+
+    /**
      * 指定されたjspの呼び出しを行う
      * @param target 遷移先jsp画面のファイル名(拡張子を含まない)
      * @throws ServletException
      * @throws IOException
      */
+    protected void forward(ForwardConst target) throws ServletException, IOException {
 
-    protected void forward(ForwardConst target) throws ServletException,IOException {
-        // jspファイルの相対パスを作成
+        //jspファイルの相対パスを作成
         String forward = String.format("/WEB-INF/views/%s.jsp", target.getValue());
         RequestDispatcher dispatcher = request.getRequestDispatcher(forward);
 
-        // jspファイルの呼び出し
+        //jspファイルの呼び出し
         dispatcher.forward(request, response);
+
     }
 
-    /*
+    /**
      * URLを構築しリダイレクトを行う
      * @param action パラメータに設定する値
      * @param command パラメータに設定する値
@@ -97,63 +102,64 @@ public abstract class ActionBase {
      * @throws IOException
      */
     protected void redirect(ForwardConst action, ForwardConst command)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
 
-        // URLを構築
+        //URLを構築
         String redirectUrl = request.getContextPath() + "/?action=" + action.getValue();
-        if(command != null) {
-            redirectUrl = redirectUrl +"&command=" + command.getValue();
+        if (command != null) {
+            redirectUrl = redirectUrl + "&command=" + command.getValue();
         }
 
-        // URLへリダイレクト
+        //URLへリダイレクト
         response.sendRedirect(redirectUrl);
+
     }
 
-    /*
+    /**
      * CSRF対策 token不正の場合はエラー画面を表示
      * @return true: token有効 false: token不正
      * @throws ServletException
      * @throws IOException
      */
-
     protected boolean checkToken() throws ServletException, IOException {
 
-        // パラメータからtokenの値を取得
+        //パラメータからtokenの値を取得
         String _token = getRequestParam(AttributeConst.TOKEN);
 
-        if(_token == null || !(_token.equals(getTokenId()))) {
-            // tokenが設定されていない、またはセッションIDと一致しない場合はエラー画面を表示
+        if (_token == null || !(_token.equals(getTokenId()))) {
+
+            //tokenが設定されていない、またはセッションIDと一致しない場合はエラー画面を表示
             forward(ForwardConst.FW_ERR_UNKNOWN);
 
             return false;
         } else {
             return true;
         }
+
     }
 
-    /*
+    /**
      * セッションIDを取得する
      * @return セッションID
      */
-
     protected String getTokenId() {
         return request.getSession().getId();
     }
 
-    /*
+    /**
      * リクエストから表示を要求されているページ数を取得し、返却する
      * @return 要求されているページ数(要求がない場合は1)
      */
     protected int getPage() {
         int page;
         page = toNumber(request.getParameter(AttributeConst.PAGE.getValue()));
-        if(page == Integer.MIN_VALUE) {
+        if (page == Integer.MIN_VALUE) {
             page = 1;
         }
         return page;
     }
 
-    /*
+    /**
      * 文字列を数値に変換する
      * @param strNumber 変換前文字列
      * @return 変換後数値
@@ -168,7 +174,7 @@ public abstract class ActionBase {
         return number;
     }
 
-    /*
+    /**
      * 文字列をLocalDate型に変換する
      * @param strDate 変換前文字列
      * @return 変換後LocalDateインスタンス
@@ -180,27 +186,25 @@ public abstract class ActionBase {
         return LocalDate.parse(strDate);
     }
 
-    /*
+    /**
      * リクエストパラメータから引数で指定したパラメータ名の値を返却する
      * @param key パラメータ名
      * @return パラメータの値
      */
-
     protected String getRequestParam(AttributeConst key) {
         return request.getParameter(key.getValue());
     }
 
-    /*
+    /**
      * リクエストスコープにパラメータを設定する
      * @param key パラメータ名
      * @param value パラメータの値
      */
-
     protected <V> void putRequestScope(AttributeConst key, V value) {
         request.setAttribute(key.getValue(), value);
     }
 
-    /*
+    /**
      * セッションスコープから指定されたパラメータの値を取得し、返却する
      * @param key パラメータ名
      * @return パラメータの値
@@ -210,8 +214,7 @@ public abstract class ActionBase {
         return (R) request.getSession().getAttribute(key.getValue());
     }
 
-
-    /*
+    /**
      * セッションスコープにパラメータを設定する
      * @param key パラメータ名
      * @param value パラメータの値
@@ -220,7 +223,7 @@ public abstract class ActionBase {
         request.getSession().setAttribute(key.getValue(), value);
     }
 
-    /*
+    /**
      * セッションスコープから指定された名前のパラメータを除去する
      * @param key パラメータ名
      */
@@ -228,7 +231,7 @@ public abstract class ActionBase {
         request.getSession().removeAttribute(key.getValue());
     }
 
-    /*
+    /**
      * アプリケーションスコープから指定されたパラメータの値を取得し、返却する
      * @param key パラメータ名
      * @return パラメータの値
@@ -237,4 +240,5 @@ public abstract class ActionBase {
     protected <R> R getContextScope(PropertyConst key) {
         return (R) context.getAttribute(key.getValue());
     }
+
 }
--- src/main/java/services/ServiceBase.java
+++ src/main/java/services/ServiceBase.java
@@ -4,20 +4,21 @@ import javax.persistence.EntityManager;
 
 import utils.DBUtil;
 
-/*
+/**
  * DB接続に関わる共通処理を行うクラス
  */
 public class ServiceBase {
-    /*
+
+    /**
      * EntityManagerインスタンス
      */
     protected EntityManager em = DBUtil.createEntityManager();
 
-    /*
+    /**
      * EntityManagerのクローズ
      */
     public void close() {
-        if(em.isOpen()) {
+        if (em.isOpen()) {
             em.close();
         }
     }
